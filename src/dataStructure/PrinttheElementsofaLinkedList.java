8646c40e9d52927c4813656e06651aac7b8c542e
sjtamim <sjtamim.gfn@gmail.com>
2018-11-05 23:13:59 +0600

    Two problems of LinkedList is uploaded

---
+++ src/dataStructure/PrinttheElementsofaLinkedList.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dataStructure;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author tamim
+ */
+public class PrinttheElementsofaLinkedList {
+
+    static class SinglyLinkedListNode {
+
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+    
+    //Solution
+    static void printLinkedList(SinglyLinkedListNode head) {
+        SinglyLinkedListNode current = head;
+        while (current != null) {
+            System.out.println(current.data);
+            current = current.next;
+        }
+
+    }
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        SinglyLinkedList llist = new SinglyLinkedList();
+
+        int llistCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < llistCount; i++) {
+            int llistItem = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            llist.insertNode(llistItem);
+        }
+
+        printLinkedList(llist.head);
+
+        scanner.close();
+    }
+}
